b9f18fcd047c33bb17cc3201f99d164443cf8d1d
sssktk <s0takt0k@yahoo.co.jp>
2023-02-14 16:07:29 -0500

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Sota Kataoka on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClockApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ClockApp
+//
+//  Created by Sota Kataoka on 2/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
