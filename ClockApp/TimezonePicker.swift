df21c0c2d01d6b96cb3cc6119e2b534c1ceb9cb8
sssktk <s0takt0k@yahoo.co.jp>
2023-06-06 23:52:18 +0900

    customizing the search of TimeZone. You now can find New York timezone with input of  "New York" rather than  "New_York"

--- ClockApp/TimezonePicker.swift
+++ ClockApp/TimezonePicker.swift
@@ -1,42 +1,42 @@
+////
+////  TimezonePicker.swift
+////  ClockApp
+////
+////  Created by Sota Kataoka on 6/1/23.
+////
 //
-//  TimezonePicker.swift
-//  ClockApp
+//import SwiftUI
 //
-//  Created by Sota Kataoka on 6/1/23.
+//struct TimezonePicker: View {
+//    @Binding var selectedTimezone: TimeZone
 //
-
-import SwiftUI
-
-struct TimezonePicker: View {
-    @Binding var selectedTimezone: TimeZone
-
-    var body: some View {
-        VStack {
-            TextField("Search...", text: $searchText)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-
-//            List(timezones) { timezone in
-//                Text(timezone.identifier)
-//                    .onTapGesture {
-//                        self.selectedTimezone = timezone
-//                    }
-//            }
-        }
-        .onAppear {
-            self.timezones = TimeZone.knownTimeZoneIdentifiers.compactMap { TimeZone(identifier: $0) }
-        }
-    }
-
-    @State private var searchText = ""
-    @State private var timezones: [TimeZone] = []
-
-    private func filterTimezones() {
-        if searchText.isEmpty {
-            self.timezones = TimeZone.knownTimeZoneIdentifiers.compactMap { TimeZone(identifier: $0) }
-        } else {
-            self.timezones = TimeZone.knownTimeZoneIdentifiers.compactMap { TimeZone(identifier: $0) }
-                .filter { $0.identifier.lowercased().contains(searchText.lowercased()) }
-        }
-    }
-}
+//    var body: some View {
+//        VStack {
+//            TextField("Search...", text: $searchText)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                .padding(.horizontal)
+//
+////            List(timezones) { timezone in
+////                Text(timezone.identifier)
+////                    .onTapGesture {
+////                        self.selectedTimezone = timezone
+////                    }
+////            }
+//        }
+//        .onAppear {
+//            self.timezones = TimeZone.knownTimeZoneIdentifiers.compactMap { TimeZone(identifier: $0) }
+//        }
+//    }
+//
+//    @State private var searchText = ""
+//    @State private var timezones: [TimeZone] = []
+//
+//    private func filterTimezones() {
+//        if searchText.isEmpty {
+//            self.timezones = TimeZone.knownTimeZoneIdentifiers.compactMap { TimeZone(identifier: $0) }
+//        } else {
+//            self.timezones = TimeZone.knownTimeZoneIdentifiers.compactMap { TimeZone(identifier: $0) }
+//                .filter { $0.identifier.lowercased().contains(searchText.lowercased()) }
+//        }
+//    }
+//}
